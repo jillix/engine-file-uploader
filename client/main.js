ce8ff5ca7478cbf2f551c77343725eea06e3cbc3
Ionică Bizău <bizauionica@yahoo.com>
2015-03-18 15:56:00 +0200

    Dropzone handlers

--- client/main.js
+++ client/main.js
@@ -1,3 +1,25 @@
+var $ = require("/jquery");
+
 exports.init = function () {
-    debugger
+    var config = this._config;
+
+    var $zone = $(config.ui.zone);
+    var $previews = $(config.ui.previews);
+    var $clickable = $(config.ui.clickable);
+
+    var url = config.url;
+
+    this.dz = new Dropzone($zone.get(0), {
+        url: url || "/",
+//        previewsContainer: $previews.get(0),
+        clickable: $clickable.get(0)
+    });
+
+    $previews.hide();
+
+    this.dz.on("success", function () {
+        setTimeout(function() {
+            $previews.fadeOut();
+        }, 1000);
+    });
 };
